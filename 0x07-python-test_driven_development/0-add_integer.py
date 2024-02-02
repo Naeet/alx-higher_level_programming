9634eb080567fc7365ce0557273176f367e320e2
Naeet <nathnaelmeonnen@gmail.com>
2024-02-02 05:27:01 -0800

    test driven

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,8 +9,27 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If either of a or b is a non-integer and non-float.
     """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+
+    a = convert_to_int(a)
+    b = convert_to_int(b)
+    return a + b
+
+def convert_to_int(num):
+    """Cast the data type of num parameter
+
+    Args:
+        num (:obj:`int, float`): The number to cast.
+
+    Returns:
+        int: The number casted to integer.
+    """
+    if type(num) is float:
+        num = int(num)
+        return num
+
+    return num
