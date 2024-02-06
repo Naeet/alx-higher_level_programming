ea23cdcaba72a0b9452bf161111e6df36848fe14
Naeet <nathnaelmeonnen@gmail.com>
2024-02-06 05:02:49 -0800

    -m

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,4 @@ def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
 
     with open(filename, encoding="utf-8") as n:
-        for line in n:
-            print(n.read(), end="")
+        print(n.read(), end="")
