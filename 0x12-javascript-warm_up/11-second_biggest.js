2d6b16ae65a2e811fb8db3d4bb8687d3d978dc55
Naeet <nathnaelmeonnen@gmail.com>
2024-06-18 06:03:43 -0700

    done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,8 +6,8 @@ let result = 0;
 let finalArray = [];
 
 if (args.length > 1) {
-	  finalArray = [...new Set(args.map((e) => parseInt(e)).sort((a, b) => b - a))];
-	  result = finalArray.length > 1 ? finalArray[1] : finalArray[0];
+  finalArray = [...new Set(args.map((e) => parseInt(e)).sort((a, b) => b - a))];
+  result = finalArray.length > 1 ? finalArray[1] : finalArray[0];
 }
 
 console.log(result);
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@
 const { argv } = require('process');
 const num = parseInt(argv[2]);
 const printC = (cuantity) => {
-	  for (; cuantity > 0; cuantity--) console.log('C is fun');
+  for (; cuantity > 0; cuantity--) console.log('C is fun');
 };
 
 Number.isInteger(num) ? printC(num) : console.log('Missing number of occurrences');
